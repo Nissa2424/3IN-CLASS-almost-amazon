1083ff60e2a4a7c87108c98eb3457c11f9c53a04
Nissa McKinney <nissa2424@gmail.com>
2021-08-30 10:38:41 -0500

    createread

--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,38 +1,87 @@
 import addBookForm from '../components/forms/addBookForm';
+import {
+  createBook,
+  /* deleteBook, */
+  getSingleBook,
+  updateBook
+} from '../helpers/data/bookData';
+import { showBooks } from '../components/books';
+import { showAuthors } from '../components/authors';
+import { createAuthor } from '../helpers/data/authorData';
 
 const domEvents = () => {
-  document.querySelector('body').addEventListener('click', (e) => {
+  document.querySelector('#main-container').addEventListener('click', (e) => {
     // CLICK EVENT FOR DELETING A BOOK
-    if (e.target.id.includes('delete-book')) {
+    /* if (e.target.id.includes('delete-book')) {
+      // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
-        console.warn('CLICKED DELETE BOOK', e.target.id);
+        // console.warn('CLICKED DELETE BOOK', e.target.id);
+        // console.warn(e.target.id.split('--'));
+        const [, id] = e.target.id.split('--');
+
+        deleteBook(id).then(showBooks);
       }
-    }
+    }  */
 
     // CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
-      console.warn('CLICKED ADD BOOK BUTTON', e.target.id);
       addBookForm();
     }
 
     // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
     if (e.target.id.includes('submit-book')) {
-      console.warn('CLICKED SUBMIT BOOK', e.target.id);
+      e.preventDefault();
+      const bookObject = {
+        title: document.querySelector('#title').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        sale: document.querySelector('#sale').checked,
+        author_id: document.querySelector('#author_id').value
+      };
+
+      createBook(bookObject).then((booksArray) => showBooks(booksArray));
     }
 
-    // CLICK EVENT FOR SHOWING MODAL FORM FOR ADDING A BOOK
+    // CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('CLICKED EDIT BOOK', e.target.id);
+      const [, id] = e.target.id.split('--');
+
+      getSingleBook(id).then((bookObj) => addBookForm(bookObj));
     }
 
     // CLICK EVENT FOR EDITING A BOOK
     if (e.target.id.includes('update-book')) {
-      console.warn('CLICKED EDIT BOOK', e.target.id);
+      e.preventDefault();
+      const [, firebaseKey] = e.target.id.split('--');
+      const bookObject = {
+        title: document.querySelector('#title').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        sale: document.querySelector('#sale').checked,
+        author_id: document.querySelector('#author_id').value,
+        firebaseKey
+      };
+
+      updateBook(bookObject).then(showBooks);
     }
 
     // ADD CLICK EVENT FOR DELETING AN AUTHOR
     // ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
+    /* if (e.target.id.includes('add-author-btn')) {
+      addAuthorForm();
+    } */
+
     // ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
+    if (e.target.id.includes('submit-author')) {
+      e.preventDefault();
+      const authorObject = {
+        email: document.querySelector('#email').value,
+        first_name: document.querySelector('#first_name').value,
+        last_name: document.querySelector('#last_name').value,
+      };
+
+      createAuthor(authorObject).then(showAuthors);
+    }
     // ADD CLICK EVENT FOR EDITING AN AUTHOR
   });
 };
